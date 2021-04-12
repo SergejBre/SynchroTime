fe5e83ba0ca0fdd873822373f8fcd7a1ab4be39a
Sergej <sergej1@email.ua>
2021-04-12 13:25:32 +0200

    Updated documentation

---
+++ doc/html/search/variables_e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ui',['ui',['../classMainWindow.html#a35466a70ed47252a0191168126a352a5',1,'MainWindow::ui()'],['../classSettingsDialog.html#aef76017ec1056d6e88e6bb48cdf73b0f',1,'SettingsDialog::ui()']]]
+];
