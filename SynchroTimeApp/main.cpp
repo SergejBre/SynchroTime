cd4cf703552a04eb0eaceb78210339327d6baa69
Sergej <sergej1@email.ua>
2021-06-09 11:40:02 +0200

    Updated version of the application to a new release

--- SynchroTimeApp/main.cpp
+++ SynchroTimeApp/main.cpp
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     app.setApplicationName( QStringLiteral( "SynchroTime" ));
-    app.setApplicationVersion( QStringLiteral( "v1.1.5, built on: " ) + QString(__DATE__).simplified() + " " + __TIME__ );
+    app.setApplicationVersion( QStringLiteral( "v1.1.5.12, built on: " ) + QString(__DATE__).simplified() + " " + __TIME__ );
     app.setWindowIcon( QIcon( QStringLiteral( "../images/icon.png") ));
 #ifdef Q_OS_WIN
     app.addLibraryPath( QLibraryInfo::location( QLibraryInfo::LibrariesPath ));
